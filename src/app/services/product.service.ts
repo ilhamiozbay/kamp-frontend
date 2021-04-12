f30f5135e324c4cfff85023904211bf5490d66fe
İlhami ÖZBAY <ilhami.ozbay@gmail.com>
2021-04-13 00:40:49 +0300

    angular-2

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from '../models/productResponseModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  apiUrl = 'https://localhost:44323/api/Products/getall';
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts(): Observable<ProductResponseModel> {
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  }
+}
